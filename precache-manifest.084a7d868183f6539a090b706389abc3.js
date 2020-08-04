154361f990b86607ad4ab02227d669a31b73e079
Kiran Goel <kiran.t.goel@gmail.com>
2020-08-04 01:56:17 -0400

    Updates

---
+++ precache-manifest.084a7d868183f6539a090b706389abc3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "033b8727081e34e4affe0af9215e26e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae98ad69dc65033c805c",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "def61abd15d09e78f7d7",
+    "url": "/static/css/main.0bd99870.chunk.css"
+  },
+  {
+    "revision": "ae98ad69dc65033c805c",
+    "url": "/static/js/2.99a1f544.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.99a1f544.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "def61abd15d09e78f7d7",
+    "url": "/static/js/main.622aaff1.chunk.js"
+  },
+  {
+    "revision": "51ff1df8d02dd786cb92",
+    "url": "/static/js/runtime-main.3ce2ee87.js"
+  },
+  {
+    "revision": "71ec907c8f2cb5f2b7f08c8777e4da88",
+    "url": "/static/media/banner.71ec907c.jpg"
+  }
+]);
