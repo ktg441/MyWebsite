4cd4ed074be1a09bc454a0d7374f56675d3afda1
Kiran Thomas Payne <kiran.t.goel@gmail.com>
2020-09-22 00:38:03 -0400

    Added svg background of mountains

--- src/App.js
+++ src/App.js
@@ -1,32 +1,13 @@
 import React, { Component } from 'react';
-
 import "./css/App.css";
 
-import Banner from "./media/banner.jpg"
 //Import various components and/or css files
 
 class App extends Component {
     render() {
         return (
             <React.Fragment>
-                <div className = "Banner">
-                    <img 
-                    src = {Banner} 
-                    className = "img-fluid"
-                    alt = "Purple Horizon"
-                    />
-                    <div className = "OnBanner">
-                        <p className = "h1 font-italic">
-                            Kiran Thomas Payne
-                        </p>
-                        <p className = "h2 text-muted font-italic">
-                            Current Title
-                        </p>
-                    </div>
-                </div>
-                <main className = "container">
-
-                </main>
+                <object className="backgroundImg" aria-label="polite" ></object>
             </React.Fragment>
         );
     }
