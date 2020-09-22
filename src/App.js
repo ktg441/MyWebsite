23e8863aa850d8bc0152d29facff09071356592f
Kiran Thomas Payne <kiran.t.goel@gmail.com>
2020-09-22 01:19:55 -0400

    Added animated particle effects

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
 import "./css/App.css";
+import "./css/lights.scss";
 
 //Import various components and/or css files
 
 class App extends Component {
+
+    createLightParticles = () => {
+        //console.log("hi");
+        var lights = [];
+        for (var i = 0; i < 100; i++) {
+            lights.push(
+            <div key ={"outerCircle" + i} className="circle-container">
+                <div key={"innerCircle" + i} className="circle"></div>
+            </div>
+            );
+        }
+
+        return <div id="lightsContainer" className="lightsContainer" >{lights}</div>;
+    }
+
     render() {
         return (
             <React.Fragment>
                 <object className="backgroundImg" aria-label="polite" ></object>
+                { this.createLightParticles() } 
             </React.Fragment>
         );
     }
