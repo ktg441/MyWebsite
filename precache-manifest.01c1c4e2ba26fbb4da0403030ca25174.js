8b614708eb2005ef03312c57d64af6eb84b42e8f
Kiran Thomas Payne <kiran.t.goel@gmail.com>
2020-09-23 19:09:27 -0400

    Updates

---
+++ precache-manifest.01c1c4e2ba26fbb4da0403030ca25174.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb231ed781d02a2bcd058dba1bafa383",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f8eba79ad3309c1bf7e6",
+    "url": "/static/css/2.6e90b12a.chunk.css"
+  },
+  {
+    "revision": "9d3cae17d149a7695666",
+    "url": "/static/css/main.1edc21fd.chunk.css"
+  },
+  {
+    "revision": "f8eba79ad3309c1bf7e6",
+    "url": "/static/js/2.7ccbd272.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7ccbd272.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d3cae17d149a7695666",
+    "url": "/static/js/main.056c728a.chunk.js"
+  },
+  {
+    "revision": "420d11f43645d04fc865",
+    "url": "/static/js/runtime-main.9b126251.js"
+  },
+  {
+    "revision": "89f7266d2a3897b5ed46505e70d4a89d",
+    "url": "/static/media/Logo.89f7266d.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.96e3c474e20dae898c8d9d7c6d8e88a3.js"
+  "/precache-manifest.01c1c4e2ba26fbb4da0403030ca25174.js"
 );
 
 self.addEventListener('message', (event) => {
