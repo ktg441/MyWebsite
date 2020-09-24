c5c29c12c7dec8f6c73174557ab555c4ebf58e8e
Kiran Thomas Payne <kiran.t.goel@gmail.com>
2020-09-24 16:53:36 -0400

    Added navbar links and social media

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
 
 import Logo from "../media/Logo.png";
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 /*
 Component that contains a navbar for site navigation
@@ -22,15 +25,22 @@ class CustomNavbar extends Component {
     render() {
         return (
             <Container fluid>
-                <Navbar collapseOnSelect fixed="top" bg="#000000" expand="lg">
-                    <Navbar.Brand href="#home">
-                        <img src={Logo} width="60" height="60" alt="" className="d-inline-block align-top" />{' '}
+                <Navbar collapseOnSelect fixed="top">
+                    <Navbar.Brand href="#about-me">
+                        <img src={Logo} width="60" height="60" alt="" className="d-inline-block align-center" />{' '}
                         Kiran Thomas Payne
                     </Navbar.Brand>
+                    {'|'}
+                    <Nav className="mr-auto">
+                        <Nav.Link href="#about-me">About Me</Nav.Link>
+                        <Nav.Link href="#resume">Resume</Nav.Link>
+                        <Nav.Link href="#projects">Projects</Nav.Link>
+                        <Nav.Link href="#contact">Contact</Nav.Link>
+                    </Nav>
                     <Nav>
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#features">Features</Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                        <Nav.Link href="https://www.linkedin.com/in/kiran-payne/" target="_blank"><FontAwesomeIcon icon={faLinkedin} /></Nav.Link>
+                        <Nav.Link href="https://github.com/ktg441" target="_blank"><FontAwesomeIcon icon={faGithub} /></Nav.Link>
+                        <Nav.Link href="mailto:kiran.t.payne@gmail.com, payne77@purdue.edu" target="_blank"><FontAwesomeIcon icon={faEnvelope} /></Nav.Link>
                     </Nav>
                 </Navbar>
             </Container>
