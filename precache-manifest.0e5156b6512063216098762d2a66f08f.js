f77811a710a4958a00d8af7cacbd57fa108450fc
Kiran Thomas Payne <kiran.t.goel@gmail.com>
2020-09-24 18:00:58 -0400

    Updates

---
+++ precache-manifest.0e5156b6512063216098762d2a66f08f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10a8fcf2a354fc60b6e705cf102501be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4bb0684af4e0b3e251ce",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "9f40822a904f8f67da97",
+    "url": "/static/css/main.df4b0fcc.chunk.css"
+  },
+  {
+    "revision": "4bb0684af4e0b3e251ce",
+    "url": "/static/js/2.a1ecdfc2.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.a1ecdfc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f40822a904f8f67da97",
+    "url": "/static/js/main.186bc1cb.chunk.js"
+  },
+  {
+    "revision": "420d11f43645d04fc865",
+    "url": "/static/js/runtime-main.9b126251.js"
+  },
+  {
+    "revision": "bcf25efb75b4e7fea9029533a81357fd",
+    "url": "/static/media/KiranPumpkingPatch.bcf25efb.jpg"
+  },
+  {
+    "revision": "89f7266d2a3897b5ed46505e70d4a89d",
+    "url": "/static/media/Logo.89f7266d.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.688961a25a4a8667c97ef65196f47c43.js"
+  "/precache-manifest.0e5156b6512063216098762d2a66f08f.js"
 );
 
 self.addEventListener('message', (event) => {
