7177494e0f5266e41fa1ab87111baa94ea1619c1
Kiran Thomas Payne <kiran.t.goel@gmail.com>
2020-10-26 22:25:43 -0400

    Redo contact page so components don't mess up viewport on mobile

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
 
-import '@rocktimsaikia/github-card';
-import { Card } from 'react-bootstrap';
-import { Icon } from '@iconify/react';
-import emailIcon from '@iconify/icons-fxemoji/email';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 /*
 Component about where to contact me
@@ -25,26 +23,21 @@ class Contact extends Component {
         return (
             <div id="contact" className="siteHidden">
                 <div id="contactHeader"><u>Contact Me</u></div>
-                <div id="contactRow">
-                    <div id="linkedinDiv" className="contactPane">
-                        <div className="LI-profile-badge"  data-version="v1" data-size="large" data-locale="en_US" data-type="vertical" data-theme="light" data-vanity="kiran-payne"><a className="LI-simple-link" target="_blank" rel="noopener noreferrer" href='https://www.linkedin.com/in/kiran-payne?trk=profile-badge'>Kiran Payne</a></div>
-                    </div>
-                    <div id="githubDiv" className="contactPane">
-                        <github-card data-user="ktg441"></github-card>
-                    </div>
-                    <div id="emailDiv" className="contactPane">
-                        <Card className="emailCard">
-                            <Icon className="bigMail" icon={emailIcon} />
-                            <Card.Body>
-                                <Card.Title>Click a link below to Email me!</Card.Title>
-                                <Card.Text>
-                                    <a href="mailto:kiran.t.payne@gmail.com" target="_blank" rel="noopener noreferrer">Personal Email</a>
-                                </Card.Text>
-                                <Card.Text>
-                                    <a href="mailto:payne77@purdue.edu" target="_blank" rel="noopener noreferrer">School Email</a>
-                                </Card.Text>
-                            </Card.Body>
-                        </Card>
+                <div id="githubDiv" className="contactPane">
+                    <div class="text-center card-box">
+                        <div class="member-card pt-2 pb-2">
+                            <div class="thumb-lg member-thumb mx-auto"><img src="https://avatars3.githubusercontent.com/u/47010494?s=460&u=750b9d3009fa7a9be67721740da4983a0aabb9da&v=4" class="rounded-circle img-thumbnail" alt="Profile Pic from Github" /></div>
+                            <div class="">
+                                <h4>Kiran Payne</h4>
+                                <p class="text-muted">@Developer <span>| </span><span><a href="https://ktg441.github.io/" target="_blank" rel="noopener noreferrer" class="text-pink">ktg441.github.io</a></span></p>
+                            </div>
+                            <ul class="social-links list-inline">
+                                <li class="list-inline-item"><a href="https://www.linkedin.com/in/kiran-payne/" target="_blank" rel="noopener noreferrer" title="" data-placement="top" data-toggle="tooltip" class="tooltips" data-original-title="LinkedIn"><FontAwesomeIcon icon={faLinkedin} /></a></li>
+                                <li class="list-inline-item"><a href="https://github.com/ktg441" target="_blank" rel="noopener noreferrer" title="" data-placement="top" data-toggle="tooltip" class="tooltips" data-original-title="Github"><FontAwesomeIcon icon={faGithub} /></a></li>
+                                <li class="list-inline-item"><a href="mailto:kiran.t.payne@gmail.com, payne77@purdue.edu" target="_blank" rel="noopener noreferrer" title="" data-placement="top" data-toggle="tooltip" class="tooltips" data-original-title="Email"><FontAwesomeIcon icon={faEnvelope} /></a></li>
+                            </ul>
+                            Senior CS Student with a passion for application development.
+                        </div>
                     </div>
                 </div>
             </div>    
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
-import Container from 'react-bootstrap/Container';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 
@@ -25,26 +24,26 @@ class CustomNavbar extends Component {
     render() {
         return (
             <div id="navWrapper" className="siteHidden">
-                <Container fluid>
-                    <Navbar collapseOnSelect bg="info" fixed="top" className="shadow-lg">
+                    <Navbar collapseOnSelect expand='md' bg="info" fixed="top" className="shadow rounded-pill-bottom">
                         <Navbar.Brand href="#about-me">
                             <img src={Logo} width="60" height="60" alt="" className="d-inline-block align-center" />{' '}
                             Kiran Thomas Payne
                         </Navbar.Brand>
-                        {'|'}
-                        <Nav className="mr-auto">
-                            <Nav.Link href="#about-me" className="text-dark" >About Me</Nav.Link>
-                            <Nav.Link href="#resume" className="text-dark" >Resume</Nav.Link>
-                            <Nav.Link href="#projects" className="text-dark" >Projects</Nav.Link>
-                            <Nav.Link href="#contact" className="text-dark" >Contact</Nav.Link>
-                        </Nav>
-                        <Nav>
-                            <Nav.Link href="https://www.linkedin.com/in/kiran-payne/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedin} /></Nav.Link>
-                            <Nav.Link href="https://github.com/ktg441" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub} /></Nav.Link>
-                            <Nav.Link href="mailto:kiran.t.payne@gmail.com, payne77@purdue.edu" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faEnvelope} /></Nav.Link>
-                        </Nav>
+                        <Navbar.Toggle aria-controls="collapsingNavbar" />
+                        <Navbar.Collapse id="siteNavbar">
+                            <Nav className="mr-auto">
+                                <Nav.Link href="#about-me" className="text-dark" >About Me</Nav.Link>
+                                <Nav.Link href="#resume" className="text-dark" >Resume</Nav.Link>
+                                <Nav.Link href="#projects" className="text-dark" >Projects</Nav.Link>
+                                <Nav.Link href="#contact" className="text-dark" >Contact</Nav.Link>
+                            </Nav>
+                            <Nav>
+                                <Nav.Link href="https://www.linkedin.com/in/kiran-payne/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedin} /></Nav.Link>
+                                <Nav.Link href="https://github.com/ktg441" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub} /></Nav.Link>
+                                <Nav.Link href="mailto:kiran.t.payne@gmail.com, payne77@purdue.edu" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faEnvelope} /></Nav.Link>
+                            </Nav>
+                        </Navbar.Collapse>
                     </Navbar>
-                </Container>
             </div>
         );
     }
